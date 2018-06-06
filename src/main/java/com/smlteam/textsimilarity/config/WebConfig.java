a37e282d85b76add8294bacaeae3fc27b4482fcc
Nguyễn Viết Thành Chương <nguyenvietthanhchuong@gmail.com>
2018-06-06 15:50:44 +0700

    Add uploadfile controller

--- src/main/java/com/smlteam/textsimilarity/config/WebConfig.java
+++ src/main/java/com/smlteam/textsimilarity/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.smlteam.textsimilarity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,5 +27,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/styles/**").addResourceLocations("/styles/").setCachePeriod(31556926);
         registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCachePeriod(31556926);
         registry.addResourceHandler("/scripts/**").addResourceLocations("/scripts/").setCachePeriod(31556926);
+    }
+
+    @Bean(name = "multipartResolver")
+    public MultipartResolver getMultipartResolver() {
+        CommonsMultipartResolver resover = new CommonsMultipartResolver();
+        // 1MB
+        resover.setMaxUploadSize(1 * 1024 * 1024);
+
+        return resover;
     }
 }
---
+++ src/main/java/com/smlteam/textsimilarity/controllers/FileUploadController.java
@@ -0,0 +1,4 @@
+package com.smlteam.textsimilarity.controllers;
+
+public class FileUploadController {
+}
